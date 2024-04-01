b6c18da6b65fc6a2a8123242cbac230140715c16
sean <seangodric@gmail.com>
2024-04-01 17:48:43 +0800

    init

---
+++ app/src/main/java/com/example/cc10624superhero/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.cc10624superhero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvHero;
+    String[] items;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        lvHero = (ListView) findViewById(R.id.lvHero);
+        items = getResources().getStringArray(R.array.herolist);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+
+        lvHero.setAdapter(adapter);
+
+        lvHero.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String nameHero = items[i];
+                Intent intent = new Intent(MainActivity.this, ShowHero.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("heroname", nameHero);
+                bundle.putInt("herono", i);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/cc10624superhero/ShowHero.java
@@ -0,0 +1,30 @@
+package com.example.cc10624superhero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ShowHero extends AppCompatActivity {
+
+    private TextView lblHeroName;
+    private ImageView imgHero;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_hero);
+
+        lblHeroName = (TextView) findViewById(R.id.lblHeroName);
+        imgHero = (ImageView) findViewById(R.id.imgHero);
+
+        Intent intent = getIntent();
+        String heroname = "Hero name is " + intent.getStringExtra("heroname");
+        lblHeroName.setText(heroname);
+
+        int heroindex = intent.getExtras().getInt("herono");
+        imgHero.setImageResource(getResources().getIdentifier("hero" + heroindex, "drawable", getPackageName()));
+    }
+}
